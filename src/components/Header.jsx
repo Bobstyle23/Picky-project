2980f11921eca0e8b81940d56e30791c2f23d1a7
Bob Pakhriev <boburpakhriev@gmail.com>
2021-12-18 17:43:23 +0900

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Navbar, Container, Form, Nav, FormControl } from "react-bootstrap";
+import logo from "../images/icon-picky@3x.png";
+import searchIcon from "../images/search@3x.png";
+import "./styles/header.css";
+const Header = () => {
+  return (
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#">
+            <img className="logo" src={logo} alt="" />
+          </Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse>
+            <Nav className="me-auto my-2 my-lg-0 header">
+              <Nav.Link href="#action1">Products</Nav.Link>
+              <Nav.Link className="discussion" href="#action2">
+                <p className="discussion-text">Discussions</p>
+              </Nav.Link>
+              <Nav.Link href="#action2">Blog</Nav.Link>
+              <Nav.Link href="#action2">Our Story</Nav.Link>
+            </Nav>
+            <img className="searchIcon" src={searchIcon} alt="search" />
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Products, brands, discussions"
+                className="me-2 control"
+                aria-label="Search"
+              />
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
